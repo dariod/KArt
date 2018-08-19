fdd3d259c095fc3f83d9d02a424cfc691a22f135
Dario Dorella <dario.dorella@gmail.com>
2018-08-19 14:33:58 -0700

    Removing some template fluff.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
         <div className="column bindToVH">
             <h2>bind To VH</h2>
             <p className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <Link to="/page-2/">Go to page 2</Link>
         </div>
     </div>
 )
